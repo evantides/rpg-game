0791347ce4d1042a785126600dd8176305ebe238
Evander Santana <evantually@Evanders-MacBook-Pro.local>
2020-08-02 22:56:10 -0400

    more art, more logic, more DOM

--- js/app.js
+++ js/app.js
@@ -1,51 +1,135 @@
-console.log($);
 const updateDOM = ($element, popUp) => {
 	const $modal = $("<div>");
 	$modal.addClass("modalVisible");
 	const $text = $("<p>");
 	$text.addClass("description");
-	$text.text(popUp);
+	$text.html(popUp + "<br />");
 	$(`${$element}`).append($modal);
 	$modal.append($text);
+	if ($element === "#playWindow") {
+		$modal.on("click", (event) => {
+			console.log("what");
+			let $thisThing = $(event.currentTarget);
+			$thisThing.remove();
+		});
+		setTimeout(() => {
+			$modal.remove();
+		}, 2000);
+	} else {
+		return $modal;
+	}
+};
+
+const itemDOM = ($element, popUp) => {
+	$modal = updateDOM($element, popUp);
+	const $yes = $("<button>").addClass("yes");
+	const $no = $("<button>").addClass("no");
+	$yes.text("yes");
+	$no.text("no");
+	$modal.append($yes);
+	$modal.append($no);
+	$yes.on("click", (event) => {
+		randomItemFind();
+		$modal.remove();
+	});
+	$no.on("click", () => {
+		$modal.remove();
+	});
+};
+
+const askDOM = ($element, popUp) => {
+	$modal = updateDOM($element, popUp);
+	const $fight = $("<button>").addClass("fight");
+	const $heal = $("<button>").addClass("heal");
+	const $flee = $("<button>").addClass("flee");
+	$fight.text("Fight them!");
+	$heal.text("Heal yourself!");
+	$flee.text("Risk your turn to run away!");
+	$modal.append($fight);
+	$modal.append($heal);
+	$modal.append($flee);
+	$fight.on("click", (event) => {
+		attackLoop();
+		$modal.remove();
+	});
+	$heal.on("click", (event) => {
+		console.log("you better heal yo'self");
+	});
+	$flee.on("click", (event) => {
+		console.log("run a check to see if you can.... run!");
+	});
+};
+
+const formDOM = (element, question) => {
+	const $form = $("<form>");
+	$form.addClass("in-game-form");
+	const $label = $("<label>");
+	$label.attr("for", "infoRequired");
+	$label.text(question);
+	const $input = $("<input>");
+	$input.attr("type", "text");
+	$input.attr("id", "infoRequired");
+	const $submit = $("<input>");
+	$submit.attr("type", "submit");
+	$submit.attr("value", "Submit");
+	$(`${element}`).append($form);
+	$form.append($label);
+	$form.append($input);
+	$form.append($submit);
+};
+
+const addInventory = (player, item) => {
+	const $show = $("<div>");
+	$show.addClass("inventory-container");
+	const $modal = $("<div>");
+	$modal.addClass("modal");
+	const $image = $("<img>");
+	switch (item.name) {
+		case "rusty sword":
+			$image.attr("src", "img/rusty_sword.png");
+			$image.attr("class", "sword");
+			break;
+		case "good sword":
+			$image.attr("src", "img/just_a_sword.png");
+			$image.attr("class", "sword");
+			break;
+		case "health potion":
+			$image.attr("src", "img/hp_potion.png");
+			$image.attr("class", "health");
+			break;
+	}
+	const $name = $("<h2>");
+	$name.addClass("description");
+	$name.text(item.name);
+	const $description = $("<p>");
+	$description.addClass("description");
+	$description.text(item.description);
+	$("#Inventory").append($show);
+	$show.append($image);
+	$show.append($modal);
+	$modal.append($name);
+	$modal.append($description);
+	$show.click((event) => {
+		let $thisThing = $(event.currentTarget).find(".modal");
+		$thisThing.toggle(".showIt");
+	});
+};
+
+const loadInventory = (player, thing) => {
+	if (!thing) {
+		player.inventory.forEach((item) => {
+			addInventory(player, item);
+		});
+	} else if (thing) {
+		addInventory(player, thing);
+	}
 };
 
 $(() => {
-	const loadInventory = () => {
-		player.inventory.forEach((item) => {
-			const $show = $("<div>");
-			$show.addClass("inventory-container");
-			const $modal = $("<div>");
-			$modal.addClass("modal");
-			const $image = $("<img>");
-			if (item.name === "rusty sword") {
-				$image.attr("src", "img/rusty_sword.png");
-				$image.attr("class", "sword");
-			} else if (item.name === "good sword") {
-				$image.attr("src", "img/just_a_sword.png");
-				$image.attr("class", "sword");
-			}
-			const $name = $("<h2>");
-			$name.addClass("description");
-			$name.text(item.name);
-			const $description = $("<p>");
-			$description.addClass("description");
-			$description.text(item.description);
-			$("#Inventory").append($show);
-			$show.append($image);
-			$show.append($modal);
-			$modal.append($name);
-			$modal.append($description);
-		});
-	};
-	loadInventory();
-	let chosen = null;
-	$(".inventory-container").on("click", (event) => {
-		let $thisThing = $(event.currentTarget).find(".modal");
-		chosen = $thisThing.find("h2").text();
-		$thisThing.toggle(".showIt");
-	});
-	$(".modalVisible").on("click", (event) => {
-		let $thisThing = $(event.currentTarget);
-		$thisThing.remove();
+	$("form").on("submit", (event) => {
+		event.preventDefault();
+		let name = $("#infoRequired").val();
+		start(name);
+		$(event.currentTarget).remove();
 	});
 });
--- js/logic.js
+++ js/logic.js
@@ -1,22 +1,47 @@
 class Objects {
-	constructor(type, state, name, level, theClass) {
+	constructor(type, name, level, health, accuracy) {
 		this.type = type;
-		this.state = state;
 		this.name = name;
 		this.level = level;
 		this.inventory = [];
-		this.theClass = theClass;
+		this.health = health;
+		this.accuracy = accuracy;
 	}
 	attack(target, choose) {
+		const test = Math.floor(Math.random() * (0 - 10)) / 10;
 		if (this.inventory.length === 0) {
-			console.log(`${this.name} punches ${target.name}!`);
+			if (this.accuracy >= test) {
+				target.health--;
+				updateDOM(
+					"#playWindow",
+					`${this.name} punches ${target.name}! They now have ${target.health} health left!`
+				);
+			} else {
+				updateDOM("#playWindow", `${this.name} missed ${target.name}!`);
+			}
+		} else if (this.accuracy >= test) {
+			target.health -= choose.damage;
+			choose.durability--;
+			updateDOM(
+				"#playWindow",
+				`${this.name} hits ${target.name} with a ${choose.name}! They now have ${target.health} health left, and their weapon now has ${choose.durability} hit(s) left before it breaks!`
+			);
 		} else {
-			console.log(`${this.name} hits ${target.name} with a ${choose.name}!`);
+			choose.durability--;
+			updateDOM(
+				"#playWindow",
+				`${this.name} missed ${target.name} Their weapon now has ${choose.durability} hit(s) left before it breaks!`
+			);
 		}
 	}
+	heal(healthItem) {}
 	aquireItem(item) {
 		item.owner = this.name;
 		this.inventory.push(item);
+		loadInventory(this, item);
+	}
+	startingItems() {
+		this.inventory.owner = this.name;
 	}
 }
 
@@ -24,54 +49,148 @@ class Factory {
 	constructor(level) {
 		this.badGuys = [];
 		this.level = level;
-		this.state = "alive";
 	}
-	instantiateBad() {
-		const generatedObj = new Objects(
-			"baddie",
-			this.state,
-			`badguy Number${this.creations.length}, this.level`
-		);
-		this.creations.push(generatedObj);
+	instantiateBad(num) {
+		for (let i = 0; i < this.level * num; i++) {
+			const generatedObj = new Objects(
+				"baddie",
+				`Ruffian #${this.badGuys.length + 1}`,
+				this.level,
+				Math.floor(Math.random() * (10 - 5) + 5),
+				Math.floor(Math.random() * (7 - 3)) / 10
+			);
+			this.badGuys.push(generatedObj);
+		}
+		return this.badGuys;
 	}
-	instantiatePlayer(name) {
-		const you = new Objects("player", this.state, name, 1);
+	instantiatePlayer(name, health) {
+		const you = new Objects("player", name, 1, health, 0.6);
 		return you;
 	}
+	instantiateBoss(health) {}
 }
 
 class Item {
-	constructor(name, damage, type, level, description) {
+	constructor(name, damage, type, durability, level, description) {
 		this.name = name;
 		this.damage = damage; // if damage is positive, it damages the person its used on. If damage is negative, it heals!
 		this.type = type;
 		this.level = level;
 		this.owner = null;
-		this.durability = 5;
+		this.durability = durability;
 		this.description = description;
 	}
 }
 
-let level = 1;
-let gameLvl = new Factory(level);
-let rustySword = new Item(
-	"rusty sword",
-	2,
-	"sword",
-	1,
-	"Hopefully it's sharp enough to get the job done."
-);
-let sword = new Item(
-	"good sword",
-	5,
-	"sword",
-	1,
-	"It looks sharp, and durable!"
-);
+class Item_Factory {
+	constructor(level) {
+		this.weapons = [];
+		this.modifiers = [];
+		this.level = level;
+	}
+	createRustySword() {
+		const rustySword = new Item(
+			"rusty sword",
+			5,
+			"sword",
+			2,
+			1,
+			"Hopefully it's sharp enough to get the job done."
+		);
+		this.weapons.push(rustySword);
+		return rustySword;
+	}
+	createSword() {
+		const sword = new Item(
+			"good sword",
+			5,
+			"sword",
+			5,
+			1,
+			"It looks sharp, and durable!"
+		);
+		this.weapons.push(sword);
+		return sword;
+	}
+	createHealthPot() {
+		const healthPot = new Item("health potion", -3, "healing", 3, 1, "Heals!");
+		this.modifiers.push(healthPot);
+		return healthPot;
+	}
+}
 
-let player = gameLvl.instantiatePlayer("John");
-console.log(player);
-player.attack(player);
-player.aquireItem(rustySword);
-player.aquireItem(sword);
-player.attack(player, sword);
+const gameLoop = (baddies, player) => {
+	const startLoop = () => {
+		askDOM("#main", `There are ${baddies.length} bad guys left! Now what?`);
+	};
+	attackLoop = () => {
+		console.log(baddies[0].health);
+		player.attack(baddies[0], player.inventory[0]);
+		setTimeout(() => {
+			if (baddies[0].health <= 0) {
+				updateDOM("#playWindow", `${baddies[0].name} has died!`);
+				if (Math.floor(Math.random() * (10 - 5) + 5) > 7) {
+					itemDOM(
+						"#main",
+						`${baddies[0].name} had an item on him! Would you like to see what it is?`
+					);
+				} else {
+					setTimeout(startLoop, 2000);
+				}
+				baddies.shift();
+			} else {
+				baddies[0].attack(player);
+				setTimeout(
+					() => askDOM("#main", `${baddies[0].name} is still alive! Now what?`),
+					2000
+				);
+			}
+		}, 2000);
+	};
+	randomItemFind = () => {
+		const random = new Item_Factory();
+		random.createHealthPot();
+		random.createRustySword();
+		random.createSword();
+		numRand = Math.floor(Math.random() * 2);
+		if (numRand === 1) {
+			num = Math.floor(Math.random() * 2);
+			player.aquireItem(random.weapons[num]);
+			updateDOM("#playWindow", `You found a ${random.weapons[num].name}`);
+		} else {
+			player.aquireItem(random.modifiers[0]);
+			updateDOM("#playWindow", `You found a ${random.modifiers[0].name}`);
+		}
+	};
+	healYourself = () => {
+		player.heal(
+			player.inventory.find((element) => {
+				element === healthPot;
+			})
+		);
+	};
+};
+let level = 0;
+
+formDOM("#playWindow", "enter the name of your character");
+const start = (name) => {
+	level++;
+	const gameLvl = new Factory(level);
+	const player = gameLvl.instantiatePlayer(name, 20);
+	const totalItems = new Item_Factory(level);
+	player.inventory = [
+		totalItems.createRustySword(),
+		totalItems.createHealthPot(),
+	];
+	console.log(player.inventory);
+	loadInventory(player);
+
+	let baddies = gameLvl.instantiateBad(3);
+	console.log(baddies);
+	gameLoop(baddies, player);
+	// player.attack(player, player.inventory[0]);
+	askDOM(
+		"#main",
+		`There are ${baddies.length} bad guys! The first one is approaching! What do you do?`
+	);
+};
