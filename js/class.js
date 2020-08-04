067a23d90e967ae06cc526185c822535267030c2
Evander Santana <evantually@Evanders-MacBook-Pro.local>
2020-08-03 23:07:23 -0400

    More work on the project

---
+++ js/class.js
@@ -0,0 +1,152 @@
+class Objects {
+	constructor(type, name, level, health, accuracy) {
+		this.type = type;
+		this.name = name;
+		this.level = level;
+		this.inventory = [];
+		this.health = health;
+		this.accuracy = accuracy;
+	}
+	attack(target, choose) {
+		const test = Math.floor(Math.random() * (0 - 10)) / 10;
+		if (
+			this.inventory.length === 0 ||
+			(this.inventory.length > 0 &&
+				!this.inventory.find((element) => element.type === "sword"))
+		) {
+			if (this.accuracy >= test) {
+				target.health--;
+				updateDOM(
+					"#playWindow",
+					`${this.name} punches ${target.name}! They now have ${target.health} health left!`
+				);
+			} else {
+				updateDOM("#playWindow", `${this.name} missed ${target.name}!`);
+			}
+		} else if (
+			this.inventory.find((element) => element.type === "sword") &&
+			this.accuracy >= test
+		) {
+			target.health -= choose.damage;
+			choose.durability--;
+			updateDOM(
+				"#playWindow",
+				`${this.name} hits ${target.name} with a ${choose.name}! They now have ${target.health} health left, and their weapon now has ${choose.durability} hit(s) left before it breaks!`
+			);
+			if (choose.durability <= 0) {
+				removeItem(choose, this);
+			}
+		} else {
+			choose.durability--;
+			updateDOM(
+				"#playWindow",
+				`${this.name} missed ${target.name} Their weapon now has ${choose.durability} hit(s) left before it breaks!`
+			);
+			if (choose.durability <= 0) {
+				removeItem(choose, this);
+			}
+		}
+	}
+	heal(healthItem) {
+		healthItem.durability--;
+		this.health += healthItem.damage;
+		updateDOM(
+			"#playWindow",
+			`${this.name} healed themselves for  ${healthItem.damage}. Their health is now ${this.health}. Their health potion now has ${healthItem.durability} uses left before it runs out!`
+		);
+	}
+	aquireItem(item) {
+		item.owner = this.name;
+		this.inventory.push(item);
+		loadInventory(this, item);
+	}
+	startingItems() {
+		this.inventory.owner = this.name;
+	}
+}
+
+class Factory {
+	constructor(level) {
+		this.badGuys = [];
+		this.level = level;
+	}
+	instantiateBad(num) {
+		let badWeapons = new Item_Factory();
+
+		for (let i = 0; i < this.level * num; i++) {
+			badWeapons.createRustySword();
+			const generatedObj = new Objects(
+				"baddie",
+				`Ruffian #${this.badGuys.length + 1}`,
+				this.level,
+				Math.floor(Math.random() * (10 - 5) + 5),
+				Math.floor(Math.random() * (7 - 3)) / 10
+			);
+			this.badGuys.push(generatedObj);
+			this.badGuys[i].inventory.push(badWeapons.weapons[i]);
+			console.log(this.badGuys[i]);
+		}
+		return this.badGuys;
+	}
+	instantiatePlayer(name, health) {
+		const you = new Objects("player", name, 1, health, 0.6);
+		return you;
+	}
+	instantiateBoss(health) {}
+}
+
+class Item {
+	constructor(name, damage, type, durability, level, description) {
+		this.name = name;
+		this.damage = damage;
+		this.type = type;
+		this.level = level;
+		this.owner = null;
+		this.durability = durability;
+		this.description = description;
+	}
+}
+
+class Item_Factory {
+	constructor(level) {
+		this.weapons = [];
+		this.modifiers = [];
+		this.level = level;
+	}
+	createRustySword() {
+		const rustySword = new Item(
+			"rusty sword",
+			5,
+			"sword",
+			2,
+			1,
+			"Hopefully it's sharp enough to get the job done."
+		);
+		this.weapons.push(rustySword);
+		return rustySword;
+	}
+	createSword() {
+		const sword = new Item(
+			"good sword",
+			5,
+			"sword",
+			5,
+			1,
+			"It looks sharp, and durable!"
+		);
+		this.weapons.push(sword);
+		return sword;
+	}
+	createHealthPot() {
+		const healthPot = new Item("health potion", 3, "healing", 3, 1, "Heals!");
+		this.modifiers.push(healthPot);
+		return healthPot;
+	}
+	looping() {
+		for (let i = 0; i < this.level; i++) {
+			this.createHealthPot();
+			this.createSword();
+			this.createRustySword();
+		}
+	}
+}
--- js/logic.js
+++ js/logic.js
@@ -1,148 +1,3 @@
-class Objects {
-	constructor(type, name, level, health, accuracy) {
-		this.type = type;
-		this.name = name;
-		this.level = level;
-		this.inventory = [];
-		this.health = health;
-		this.accuracy = accuracy;
-	}
-	attack(target, choose) {
-		const test = Math.floor(Math.random() * (0 - 10)) / 10;
-		if (
-			this.inventory.length === 0 ||
-			(this.inventory.length > 0 &&
-				!this.inventory.find((element) => element.type === "sword"))
-		) {
-			if (this.accuracy >= test) {
-				target.health--;
-				updateDOM(
-					"#playWindow",
-					`${this.name} punches ${target.name}! They now have ${target.health} health left!`
-				);
-			} else {
-				updateDOM("#playWindow", `${this.name} missed ${target.name}!`);
-			}
-		} else if (
-			this.inventory.find((element) => element.type === "sword") &&
-			this.accuracy >= test
-		) {
-			target.health -= choose.damage;
-			choose.durability--;
-			updateDOM(
-				"#playWindow",
-				`${this.name} hits ${target.name} with a ${choose.name}! They now have ${target.health} health left, and their weapon now has ${choose.durability} hit(s) left before it breaks!`
-			);
-			if (choose.durability <= 0) {
-				removeItem(choose, this);
-			}
-		} else {
-			choose.durability--;
-			updateDOM(
-				"#playWindow",
-				`${this.name} missed ${target.name} Their weapon now has ${choose.durability} hit(s) left before it breaks!`
-			);
-			if (choose.durability <= 0) {
-				removeItem(choose, this);
-			}
-		}
-	}
-	heal(healthItem) {
-		healthItem.durability--;
-		this.health += healthItem.damage;
-		updateDOM(
-			"#playWindow",
-			`${this.name} healed themselves for  ${healthItem.damage}. Their health is now ${this.health}. Their health potion now has ${healthItem.durability} uses left before it runs out!`
-		);
-	}
-	aquireItem(item) {
-		item.owner = this.name;
-		this.inventory.push(item);
-		loadInventory(this, item);
-	}
-	startingItems() {
-		this.inventory.owner = this.name;
-	}
-}
-
-class Factory {
-	constructor(level) {
-		this.badGuys = [];
-		this.level = level;
-	}
-	instantiateBad(num) {
-		let badWeapons = new Item_Factory();
-
-		for (let i = 0; i < this.level * num; i++) {
-			badWeapons.createRustySword();
-			const generatedObj = new Objects(
-				"baddie",
-				`Ruffian #${this.badGuys.length + 1}`,
-				this.level,
-				Math.floor(Math.random() * (10 - 5) + 5),
-				Math.floor(Math.random() * (7 - 3)) / 10
-			);
-			this.badGuys.push(generatedObj);
-			this.badGuys[i].inventory.push(badWeapons.weapons[i]);
-			console.log(this.badGuys[i]);
-		}
-		return this.badGuys;
-	}
-	instantiatePlayer(name, health) {
-		const you = new Objects("player", name, 1, health, 0.6);
-		return you;
-	}
-	instantiateBoss(health) {}
-}
-
-class Item {
-	constructor(name, damage, type, durability, level, description) {
-		this.name = name;
-		this.damage = damage; // if damage is positive, it damages the person its used on. If damage is negative, it heals!
-		this.type = type;
-		this.level = level;
-		this.owner = null;
-		this.durability = durability;
-		this.description = description;
-	}
-}
-
-class Item_Factory {
-	constructor(level) {
-		this.weapons = [];
-		this.modifiers = [];
-		this.level = level;
-	}
-	createRustySword() {
-		const rustySword = new Item(
-			"rusty sword",
-			5,
-			"sword",
-			2,
-			1,
-			"Hopefully it's sharp enough to get the job done."
-		);
-		this.weapons.push(rustySword);
-		return rustySword;
-	}
-	createSword() {
-		const sword = new Item(
-			"good sword",
-			5,
-			"sword",
-			5,
-			1,
-			"It looks sharp, and durable!"
-		);
-		this.weapons.push(sword);
-		return sword;
-	}
-	createHealthPot() {
-		const healthPot = new Item("health potion", 3, "healing", 3, 1, "Heals!");
-		this.modifiers.push(healthPot);
-		return healthPot;
-	}
-}
 const removeItem = (choose, object) => {
 	object.inventory.splice(choose, 1);
 	console.log(object.inventory);
@@ -221,6 +76,7 @@ const gameLoop = (baddies, player) => {
 let level = 0;
 
 formDOM("#playWindow", "enter the name of your character");
+
 const start = (name) => {
 	level++;
 	const gameLvl = new Factory(level);
@@ -229,11 +85,20 @@ const start = (name) => {
 	player.inventory = [totalItems.createSword(), totalItems.createHealthPot()];
 	console.log(player.inventory);
 	loadInventory(player);
-	let baddies = gameLvl.instantiateBad(3);
+	let baddies = gameLvl.instantiateBad(level + 2);
 	console.log(baddies);
 	gameLoop(baddies, player);
 	askDOM(
 		"#main",
 		`There are ${baddies.length} bad guys! The first one is approaching! What do you do?`
 	);
+};
+
+const newLvl = (player) => {
+	level++;
+	player.health = 20 + 2 * level;
+	player.level = level;
+	const gameLvl = new Factory(level);
+	let baddies = gameLvl.instantiateBad(level + 2);
+	const totalItems = new Item_Factory(level);
 };
